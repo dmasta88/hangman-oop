426e2192ed22b65f479d8838ea3538686a4dfa84
Dmasta88 <tweens@inbox.ru>
2024-11-13 14:48:11 -0500

    sml fx

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
 //hangman game
-
-// Start the game
-
-// Output the word mask and the basic hangman Stage 1
-// Request to enter a letter
-// Check this letter in the array of letters that make up the word
-// If there is no letter, change the picture and decrease the number of attempts.
-
-// If there is a letter, change the word mask.
-
-// Check if there are any unsolved letters left.
-// If there are none, then the word is solved. If there are any, start this cycle again.
-
-//define('ATTEMPTS', '6'); 
-//const ATTEMPTS = 6;
 require_once "autoload.php";
 
 use classes\Game;
